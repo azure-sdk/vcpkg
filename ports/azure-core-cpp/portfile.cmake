b4c456e8331b2ff80a5d2038ad80c18c0624cdf9
azure-sdk <azuresdk@microsoft.com>
2021-04-21 18:12:47 +0000

    Daily vcpkg ports for core

--- ports/azure-core-cpp/portfile.cmake
+++ ports/azure-core-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-core_1.0.0-beta.8
-    SHA512 fd882e37332fd5f35627b94c96cfae78178d9bc551cc9a27480f2ef08d496ea2379dc8d0e6a237e902ce063ae87ed6f9e8dc0b91e2e16bd8638e839f8d9e4c2a
+    REF 2ea6ee6d54f5603426c1213ea5232512f119c566
+    SHA512 35c4766aa923840e724b028b9e3babf6ada4dfa77f74523a22ccb2f4ba0fc22e4fb73d4a6f553a55d945e6f125818242e47d296e8eb24f66428c99b000c47321
 )
 
 vcpkg_check_features(
