93779078773121c8c64a34b5dddcb4eeb6171bc6
azure-sdk <azuresdk@microsoft.com>
2022-07-18 18:32:57 +0000

    Daily vcpkg ports for core

--- ports/azure-core-tracing-opentelemetry-cpp/portfile.cmake
+++ ports/azure-core-tracing-opentelemetry-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-core-tracing-opentelemetry_1.0.0-beta.2
-    SHA512 a0697ed440a08f3814013adf476c2a1333b462a2eb96bac18c47939c54686396639935568e229dad6bbbae06f54c324da82544634c6384a2c1498f9959836b6d
+    REF cff96f8598d318bbdae8df103e078502d9ad15c2
+    SHA512 c0faaebf64af4b50d3c5a76a7c7423b4652dc33b883e3716172f7b89b9e82ab967e0f91273e00bb3d197d2e2fe07710ae2994dc17042cd55faa5140f80d88c1f
 )
 
 vcpkg_cmake_configure(
