d51ec46ba0451b5818fa3615b4416192bcc4a032
azure-sdk <azuresdk@microsoft.com>
2022-07-25 18:39:14 +0000

    Daily vcpkg ports for core

--- ports/azure-core-tracing-opentelemetry-cpp/portfile.cmake
+++ ports/azure-core-tracing-opentelemetry-cpp/portfile.cmake
@@ -1,10 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-core-tracing-opentelemetry_1.0.0-beta.2
-    SHA512 a0697ed440a08f3814013adf476c2a1333b462a2eb96bac18c47939c54686396639935568e229dad6bbbae06f54c324da82544634c6384a2c1498f9959836b6d
-    PATCHES
-        "3844.patch" # https://github.com/Azure/azure-sdk-for-cpp/pull/3844 "Removed hard dependency on opentelemetry version"
+    REF 6c74e576f212f8e7c249200f827712be01558310
+    SHA512 57ee50548366b98d3466c6169800903f249586f8dd77047125726ef8a803bfe790893d34152870047ca6dcdb013e2f51e1fdd6907da4f81454ed2d33064f11f2
 )
 
 vcpkg_cmake_configure(
