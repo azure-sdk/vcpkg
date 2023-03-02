dc9ea59727c3d5ebd20eeb6a536f8629a75bdf6f
azure-sdk <azuresdk@microsoft.com>
2023-03-02 22:19:40 +0000

    Daily vcpkg ports for keyvault

--- ports/azure-security-keyvault-administration-cpp/portfile.cmake
+++ ports/azure-security-keyvault-administration-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF e48cf927042ab828bee5bdd14ed509a8f3388300
-    SHA512 abc5afc22242adfd0fb18aa04eeea0f6c257108a0ec84a5c73f473e1e8c0acf44178ab24d7909a19615011c4f88bcb73afd32a8d319174a7fd46974c52c7fd1b
+    REF c8b028dce071cd3ae582075a5063c875a4b49bad
+    SHA512 3afc99ffa77d2b8f73a651e893acb5b1f9b7b44277ee7adc59aa241270c5a0d6df4058aebb7f0fd8135372f6f65b7f43c7ff6867387cdd42a0d4b070a9fb4f67
 )
 
 vcpkg_cmake_configure(
