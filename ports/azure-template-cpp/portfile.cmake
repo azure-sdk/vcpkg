c026dc616833fb4310942d556e503ad41113cea9
azure-sdk <azuresdk@microsoft.com>
2023-07-17 22:11:10 +0000

    Daily vcpkg ports for template

---
+++ ports/azure-template-cpp/portfile.cmake
@@ -0,0 +1,19 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Azure/azure-sdk-for-cpp
+    REF d1544744bfd78080ce49963f456591d2e0388c15
+    SHA512 1ecac619f6fab34b51a0dbc079296abd868e47dbb8193b55d129b80d2967c7e4a794bddef88d5e1e66bd1c94333c9b2d1f296fda9c4b11102ded14668cd7187b
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/sdk/template/azure-template/"
+    OPTIONS
+        -DWARNINGS_AS_ERRORS=OFF
+        -DBUILD_TESTING=OFF
+)
+
+vcpkg_cmake_install()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_cmake_config_fixup()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_copy_pdbs()
