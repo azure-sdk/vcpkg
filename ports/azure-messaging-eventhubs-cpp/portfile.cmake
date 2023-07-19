ae0c50e2669fddd8a84f7bf856785a9925c3bc99
azure-sdk <azuresdk@microsoft.com>
2023-07-19 17:41:02 +0000

    Daily vcpkg ports for eventhubs

---
+++ ports/azure-messaging-eventhubs-cpp/portfile.cmake
@@ -0,0 +1,19 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Azure/azure-sdk-for-cpp
+    REF cdfabe3cc5da51468e551796e9031e56e5637dbc
+    SHA512 5cb771a8a5a62368c3bdbc9d845c98e1120d48aca09b208cfa80728d87ebbcd04855000a28555b92d18d9faf139e606778aa7dc0682dcbf0f915beb03438c72e
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/sdk/messaging/azure-messaging-eventhubs/"
+    OPTIONS
+        -DWARNINGS_AS_ERRORS=OFF
+        -DBUILD_TESTING=OFF
+)
+
+vcpkg_cmake_install()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_cmake_config_fixup()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_copy_pdbs()
