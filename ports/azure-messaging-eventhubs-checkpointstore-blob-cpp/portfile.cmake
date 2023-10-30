738322242cafafd322e54ee58a62c7af6beec9ba
azure-sdk <azuresdk@microsoft.com>
2023-10-30 17:46:00 +0000

    Daily vcpkg ports for eventhubs

--- ports/azure-messaging-eventhubs-checkpointstore-blob-cpp/portfile.cmake
+++ ports/azure-messaging-eventhubs-checkpointstore-blob-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-messaging-eventhubs-checkpointstore-blob_1.0.0-beta.1
-    SHA512 7c55eda0c04bbc57729a7b479d8d5874b0e06927aff1916833520a3a944e63b6fceabd3565fd91549ec00157f2c4af5a87a6a2db55a5c24df611cd96572f9a08
+    REF 7d94094255fb65cf659a12003ec0b05cb0dbebe6
+    SHA512 83c5a369fb9c30334df4020cad4f34ceb1b9d8389045b74c8285e065401bbf921a31f4287e8c110aeec21dfe3b6e703c0ba5838f269291b13df59f40b3b54f63
 )
 
 if(EXISTS "${SOURCE_PATH}/sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-blob")
--- ports/azure-messaging-eventhubs-cpp/portfile.cmake
+++ ports/azure-messaging-eventhubs-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-messaging-eventhubs_1.0.0-beta.3
-    SHA512 0ad0e02755eba07408c6a837a742c0e58890a4b5f281709e1d84ea59a378c0a013068428d99e81d4972172760a3fb55a6026d34d8a558a5747c48a6b3f85e664
+    REF 7d94094255fb65cf659a12003ec0b05cb0dbebe6
+    SHA512 83c5a369fb9c30334df4020cad4f34ceb1b9d8389045b74c8285e065401bbf921a31f4287e8c110aeec21dfe3b6e703c0ba5838f269291b13df59f40b3b54f63
 )
 
 if(EXISTS "${SOURCE_PATH}/sdk/eventhubs/azure-messaging-eventhubs")
