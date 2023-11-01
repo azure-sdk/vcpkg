7d99235011ed3a927cb1eb4a12a3e6c25aedf02e
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2023-11-01 10:34:15 -0700

    Manually add azure-template-cpp

--- ports/azure-template-cpp/portfile.cmake
+++ ports/azure-template-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-template_1.0.0
-    SHA512 0
+    REF acf8926bb34f58aac5ca0756413947b35c5e8168
+    SHA512 0fa2adb2c36630e05a2483094b3f6768412a46e397a4b930f4cacbfadf8dbaecba6729aa430147349f7f955c6b7c19d6ae685d2a64e6a6d7cf2bcd35b629e661
 )
 
 if(EXISTS "${SOURCE_PATH}/sdk/template/azure-template")
