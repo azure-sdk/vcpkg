d47567cc0d0c4d6684d2e8c18da1d747490da9cb
azure-sdk <azuresdk@microsoft.com>
2024-01-17 05:57:07 +0000

    Daily vcpkg ports for tables

---
+++ ports/azure-data-tables-cpp/portfile.cmake
@@ -0,0 +1,32 @@
+# NOTE: All changes made to this file will get overwritten by the next port release.
+# Please contribute your changes to https://github.com/Azure/azure-sdk-for-cpp.
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Azure/azure-sdk-for-cpp
+    REF a0f424413612237ba969028faea18fabb3bad494
+    SHA512 13a9c1f1bbface9c32bb04e9d0df5c1a4d80f4457eff8a012e20aab9dca14ee49edce7a7612a6b8a3ccab10778a44210c6d03ceaf95e4132dc60560300429fe1
+)
+
+if(EXISTS "${SOURCE_PATH}/sdk/tables/azure-data-tables")
+  file(REMOVE_RECURSE "${SOURCE_PATH}/sdk/tables/_")
+  file(REMOVE_RECURSE "${SOURCE_PATH}/sdk/_")
+  file(REMOVE_RECURSE "${SOURCE_PATH}/_")
+
+  file(RENAME "${SOURCE_PATH}/sdk/tables/azure-data-tables" "${SOURCE_PATH}/sdk/tables/_")
+  file(RENAME "${SOURCE_PATH}/sdk/tables" "${SOURCE_PATH}/sdk/_")
+  file(RENAME "${SOURCE_PATH}/sdk" "${SOURCE_PATH}/_")
+endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/_/_/_"
+    OPTIONS
+        -DWARNINGS_AS_ERRORS=OFF
+        -DBUILD_TESTING=OFF
+)
+
+vcpkg_cmake_install()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_cmake_config_fixup()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_copy_pdbs()
