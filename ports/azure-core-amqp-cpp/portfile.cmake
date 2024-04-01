964181cc0d8a3274c66d26817ca83c768ce139b3
azure-sdk <azuresdk@microsoft.com>
2024-04-01 18:21:26 +0000

    Daily vcpkg ports for core

--- ports/azure-core-amqp-cpp/portfile.cmake
+++ ports/azure-core-amqp-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-core-amqp_1.0.0-beta.7
-    SHA512 ece00ea8a005cffba9df5e00169d7c1f4d1db75bf4acfab8a5057221c7992886df89882bd5eec146e432a0463f526cd547fa90c7430a0228441762e2ad4513e1
+    REF c4f36de7056c4d6c5c66d3fffd5dceabec0e71a6
+    SHA512 a8c5a487501ccfd8824378ed99d8dc21ebe57eba9854cb4995460a8b992012dba11bd7371665d6037271b2aa2aa66cf1f32c54b77b50b9585e12e5a49c29aace
 )
 
 if(EXISTS "${SOURCE_PATH}/sdk/core/azure-core-amqp")
--- ports/azure-core-cpp/portfile.cmake
+++ ports/azure-core-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-core_1.11.2
-    SHA512 c3606d987a25c3a50aad0a3e62cd8c0769c53d200b7b9b08dbc5d422358284335855944bcbbafb5214bba6cd84d15a395dc55b9efe9caf4f16bd3e7883dab0d2
+    REF c4f36de7056c4d6c5c66d3fffd5dceabec0e71a6
+    SHA512 a8c5a487501ccfd8824378ed99d8dc21ebe57eba9854cb4995460a8b992012dba11bd7371665d6037271b2aa2aa66cf1f32c54b77b50b9585e12e5a49c29aace
 )
 
 vcpkg_check_features(
