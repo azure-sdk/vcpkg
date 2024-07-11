2def0179ddfc04e9032d280767f94e7c17762b28
azure-sdk <azuresdk@microsoft.com>
2024-07-11 20:16:57 +0000

    Daily vcpkg ports for keyvault

--- ports/azure-security-keyvault-keys-cpp/portfile.cmake
+++ ports/azure-security-keyvault-keys-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-security-keyvault-keys_${VERSION}"
-    SHA512 758ff05ce33672d81cf14d347edada2a7c008c7df61639b7bac9ce1a1114144a1f6fcc3d364d1d90f5f9cda8f8c1db14f80873403d4190d4aca1844fb25dd517
+    REF 1143b6bf4dc1eb3ffb9cf5ba4cb7ebb22f5ff073
+    SHA512 5e54c08a605214066f8cfdaf001a9e8db82987b148a2d3bdcb22627a25e892be8f4b89a143e51930d2013535cfbb193ef13016e05b4fe7f98e29dbf4dc4fce13
     HEAD_REF main
 )
 
