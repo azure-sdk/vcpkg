95c90f1e4a13430f88277c509a79ac65c074b2f9
azure-sdk <azuresdk@microsoft.com>
2024-08-01 18:15:02 +0000

    Daily vcpkg ports for core

--- ports/azure-core-amqp-cpp/portfile.cmake
+++ ports/azure-core-amqp-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-core-amqp_${VERSION}"
-    SHA512 8c3b5e06370ec1ae6b10bcd6d084687a8ac7b2bf35c7c4bf55341d8b4b913a59689b8be5fbaec725af63cbc981c757fff0de0f6a27ed2674145ed184a66dfd2f
+    REF 20e3bd3ff442f0ce018e9f99542ba22704b1d091
+    SHA512 772ea683123251d63b92c9a6090603de237e39dcfe8fb0811bce1923bc996eed9792c719b026480558a6efb07e4edac82f7474dc41d7531f694ec5af3247ccc2
     HEAD_REF main
 )
 
--- ports/azure-core-tracing-opentelemetry-cpp/portfile.cmake
+++ ports/azure-core-tracing-opentelemetry-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-core-tracing-opentelemetry_${VERSION}"
-    SHA512 645c616fe54024f30a6e4b9c2626bfeaf906086be7bbeccc4a1155178670fb70d9818938db2f9fa8e3b9593ca45e0b10042dcd67fde04d2542f6f72a74884697
+    REF 20e3bd3ff442f0ce018e9f99542ba22704b1d091
+    SHA512 772ea683123251d63b92c9a6090603de237e39dcfe8fb0811bce1923bc996eed9792c719b026480558a6efb07e4edac82f7474dc41d7531f694ec5af3247ccc2
     HEAD_REF main
 )
 
