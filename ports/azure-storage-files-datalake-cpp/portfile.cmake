79b2b3fc9f567350b60f5781affade590af0c7b1
azure-sdk <azuresdk@microsoft.com>
2024-09-30 21:15:27 +0000

    Daily vcpkg ports for storage

--- ports/azure-storage-files-datalake-cpp/portfile.cmake
+++ ports/azure-storage-files-datalake-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-storage-files-datalake_${VERSION}"
-    SHA512 fa91118b8f17c6c2800610833f0a6fbb6543a1470d8cdffd1d654516252df62f54496d9dd9756ab5ee0772230dbf58873819fc07a642fdc24af32d255f525de5
+    REF 171386533aba7cee966a50daff9735fe2431a1db
+    SHA512 faaa925ff7abb8729c5e818ec1d931c0f17b2b24e2e2f80b3de42264225b50017be85200fc41c81691c42d4677003e30144ab436802e99f0d5eecf1ff702df87
     HEAD_REF main
 )
 
