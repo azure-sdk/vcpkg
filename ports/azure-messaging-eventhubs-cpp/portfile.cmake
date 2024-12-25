db93b58861097fdb2ef99c22074a8d900da077d5
azure-sdk <azuresdk@microsoft.com>
2024-12-25 18:44:06 +0000

    Daily vcpkg ports for eventhubs

--- ports/azure-messaging-eventhubs-cpp/portfile.cmake
+++ ports/azure-messaging-eventhubs-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-messaging-eventhubs_${VERSION}"
-    SHA512 13f1ad09a4093ab3a591208e47d6ce5daad2a4e055f2c86fdb49a9dbee4826efb19ddb294c0d5d9a4e5f4e2318059bd57fc346fec5ae1cee26f20839030f4276
+    REF 049b7a6fe699334d8c03b32cd459357b90c2c27f
+    SHA512 5277907a405ed229846c6b50e33be3873b778bcacf76925e454f5c75ba39b4f1a2e7461f40e0b2896b8267de274107a0078fd533929063c2aebc99cba5c097ac
     HEAD_REF main
 )
 
