035304de3b400f63f58856da4ef98043e1065ec3
azure-sdk <azuresdk@microsoft.com>
2025-05-15 11:48:30 +0000

    Daily vcpkg ports for attestation

--- ports/azure-security-attestation-cpp/portfile.cmake
+++ ports/azure-security-attestation-cpp/portfile.cmake
@@ -4,13 +4,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-security-attestation_${VERSION}"
-    SHA512 bf5c0ab830122838045e3e5ff03aae38f81082b50b9b0c61fa21a8c101a2fd98ad98b136d49702665d720f59baa6a89c8af3f161a44c09d24ad747fb1828cbca
+    REF 77eee127178a3b53cd4c3f32802915f50b62c23d
+    SHA512 8089e301df92c2cdb69c92a8c211275a8cf81b66d311e11650d01e9cd621d0f595d38bdf472f1a82eb9733ecff2a635f081288d5e12cb29a730aef65b2bfe7d0
     HEAD_REF main
-    PATCHES
-        context_syntax.patch
-        useragent.patch
-        set_version.patch
 )
 
 if(EXISTS "${SOURCE_PATH}/sdk/attestation/azure-security-attestation")
