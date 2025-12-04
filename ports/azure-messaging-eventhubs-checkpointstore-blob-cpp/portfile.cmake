45833e4bbaab4f19ee628c5f65e16bf2b0998b12
azure-sdk <azuresdk@microsoft.com>
2025-12-04 18:56:15 +0000

    Daily vcpkg ports for eventhubs

--- ports/azure-messaging-eventhubs-checkpointstore-blob-cpp/portfile.cmake
+++ ports/azure-messaging-eventhubs-checkpointstore-blob-cpp/portfile.cmake
@@ -4,12 +4,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-messaging-eventhubs-checkpointstore-blob_${VERSION}"
-    SHA512 7c55eda0c04bbc57729a7b479d8d5874b0e06927aff1916833520a3a944e63b6fceabd3565fd91549ec00157f2c4af5a87a6a2db55a5c24df611cd96572f9a08
+    REF be8cf03ed02e12e84eecbce8d3334e5badebcfcb
+    SHA512 1fb9ddefa05ce889863b82c1664cdee17b3f3893c76e0f77a5a117578e2ba9ba84ff6f1d9f6355a20e6b814df5d797d2c0696b8b7a1506f80a9e3244b003db8c
     HEAD_REF main
-    PATCHES
-      useragent.patch
-      set_version.patch
 )
 
 file(GLOB_RECURSE unused "${SOURCE_PATH}/cgmanifest.json")
--- ports/azure-messaging-eventhubs-cpp/portfile.cmake
+++ ports/azure-messaging-eventhubs-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-messaging-eventhubs_${VERSION}"
-    SHA512 13f1ad09a4093ab3a591208e47d6ce5daad2a4e055f2c86fdb49a9dbee4826efb19ddb294c0d5d9a4e5f4e2318059bd57fc346fec5ae1cee26f20839030f4276
+    REF be8cf03ed02e12e84eecbce8d3334e5badebcfcb
+    SHA512 1fb9ddefa05ce889863b82c1664cdee17b3f3893c76e0f77a5a117578e2ba9ba84ff6f1d9f6355a20e6b814df5d797d2c0696b8b7a1506f80a9e3244b003db8c
     HEAD_REF main
 )
 
