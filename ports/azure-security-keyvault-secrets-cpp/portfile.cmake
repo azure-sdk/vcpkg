cfd3e707a242adf43c59dfe22cd2d9bffdc52046
azure-sdk <azuresdk@microsoft.com>
2025-12-19 21:26:58 +0000

    Daily vcpkg ports for keyvault

--- ports/azure-security-keyvault-secrets-cpp/portfile.cmake
+++ ports/azure-security-keyvault-secrets-cpp/portfile.cmake
@@ -4,12 +4,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-security-keyvault-secrets_${VERSION}"
-    SHA512 8d944bbcc29b670b884234a789455bb6b33e33fa45c7b02a4200c2267189597cb849f3cbbaa5f7c4db013ec74a2ea0b073237254db7fcac96c290888c05dcba5
+    REF 0e61fe4f8d79fe2aaa742c7b770219d6c3efd8c7
+    SHA512 48d55a7c2ce103481a87e9e5473fcacc0dcbffb17b85255e9c683d5bc3dfe356194a7d9a2f42a9495966ad8e4ca9e71fc8abc2b008331418ac0f05b2b8307051
     HEAD_REF main
-    PATCHES
-      useragent.patch
-      set_version.patch
 )
 
 file(GLOB_RECURSE unused "${SOURCE_PATH}/cgmanifest.json")
