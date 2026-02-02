433b010329322c45d5375e3095c6d427c8e2a838
azure-sdk <azuresdk@microsoft.com>
2026-02-02 19:23:51 +0000

    Daily vcpkg ports for core

--- ports/azure-core-tracing-opentelemetry-cpp/portfile.cmake
+++ ports/azure-core-tracing-opentelemetry-cpp/portfile.cmake
@@ -4,12 +4,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-core-tracing-opentelemetry_${VERSION}"
-    SHA512 645c616fe54024f30a6e4b9c2626bfeaf906086be7bbeccc4a1155178670fb70d9818938db2f9fa8e3b9593ca45e0b10042dcd67fde04d2542f6f72a74884697
+    REF f36292e6eaac026829346ee9a932a52bbc53c6d0
+    SHA512 2024232351efd5b16106925dd6a1be39a7866b941ea811da09558394dae62dd2e340e3d4a79499d96f1c8d2f5e9c57d2c85bd138d1df7563da957e89d3c21de9
     HEAD_REF main
-    PATCHES
-      useragent.patch
-      set_version.patch
 )
 
 file(GLOB_RECURSE unused "${SOURCE_PATH}/cgmanifest.json")
