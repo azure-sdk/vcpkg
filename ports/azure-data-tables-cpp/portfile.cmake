7ef7fa209559c7ad4794e10030deeab0f1f01d51
azure-sdk <azuresdk@microsoft.com>
2026-02-11 06:10:03 +0000

    Daily vcpkg ports for tables

--- ports/azure-data-tables-cpp/portfile.cmake
+++ ports/azure-data-tables-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-data-tables_${VERSION}"
-    SHA512 368ad9a26e824de2f547263ce923e6ccdcadaadf43bdbbffe7d88948b2f9012f5f545a073caa4311d3ff5c5486ceb0b849a5e130cff295926fbed17153d959a5
+    REF 04f2ce086af80d41892bfffd645c891b36c98961
+    SHA512 1abe4fc6dd4a9e59a7fc3049eeb21d5ecf324509d9bae2054d0c9df7b20a1fc19d1357999e7440679c686aa6be79d3ae7515d1e541f2afcb4acba531108d2d76
     HEAD_REF main
 )
 
