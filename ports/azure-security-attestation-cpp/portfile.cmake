61813ac662e690ccc594ee4b8c45bce0adbc7701
azure-sdk <azuresdk@microsoft.com>
2026-03-05 12:52:34 +0000

    Daily vcpkg ports for attestation

--- ports/azure-security-attestation-cpp/portfile.cmake
+++ ports/azure-security-attestation-cpp/portfile.cmake
@@ -4,14 +4,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-security-attestation_${VERSION}"
-    SHA512 bf5c0ab830122838045e3e5ff03aae38f81082b50b9b0c61fa21a8c101a2fd98ad98b136d49702665d720f59baa6a89c8af3f161a44c09d24ad747fb1828cbca
+    REF 22c897d1e345fc3bababfa991deff9f1364e00e1
+    SHA512 33b0017c0aaeac0f6a6b1c2dd016c6fc1f7d9002aceb970bb8bcbe97d92ccdfcbd65d17e15633ed22f0a4bee1dce23de283a8abbebc4b755918891d52c81d496
     HEAD_REF main
-    PATCHES
-        context_syntax.patch
-        useragent.patch
-        set_version.patch
-        cstdint.patch
 )
 
 file(GLOB_RECURSE unused "${SOURCE_PATH}/cgmanifest.json")
