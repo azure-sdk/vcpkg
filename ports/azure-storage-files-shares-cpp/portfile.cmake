309169fa74b1ac7d627fa11ce527c832e3e831f1
azure-sdk <azuresdk@microsoft.com>
2026-04-08 21:28:10 +0000

    Daily vcpkg ports for storage

--- ports/azure-storage-files-shares-cpp/portfile.cmake
+++ ports/azure-storage-files-shares-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-storage-files-shares_${VERSION}"
-    SHA512 cdd430d88671c28944af2d436f9e87fb56bd6cb5b1e9ce54c92a0cec5237310cdf0d9ccd6e59bae25daa8896f720ffde05697b71fbc94951df33eeae568528e3
+    REF e9e21cebb50e6a7c837937e2d3e8577503caf0b8
+    SHA512 3d7bb86f4ee947448276364b7d81f2f124669c0ad35b7765d310f905e6086c1a50dffe629485d5c167f06991b01a5cd644a81c74d9b2fff1c2d38897d3b797a7
     HEAD_REF main
 )
 
