f5ae96739c480ba8519c1dc669a20695fbb73821
azure-sdk <azuresdk@microsoft.com>
2026-06-15 19:27:32 +0000

    Daily vcpkg ports for identity

--- ports/azure-identity-cpp/portfile.cmake
+++ ports/azure-identity-cpp/portfile.cmake
@@ -4,18 +4,18 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-identity_${VERSION}"
-    SHA512 df55c714eff596ba585ac1f9ec32a6c6cc805022ac5aa05f36a342e5e967b2fb8afa80d243206c4facf3dcf1d5e921d7eed8623de5fb515faaa43c696c972396
+    REF 4bf3eb9c7039df98fe827b8c799823ae926b2bf6
+    SHA512 454d9a5da38faff97a9722b607d138ed1a7a63960fb6f66e69cb5958cc2d64ab4899bd7947423c282a2f21b31933fba15e2832e5854327d4d82fa8d5dddc8360
     HEAD_REF main
 )
 
 file(GLOB_RECURSE unused "${SOURCE_PATH}/cgmanifest.json")
 file(REMOVE_RECURSE ${unused})
 
-file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.toml")  
+file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.toml")
 file(REMOVE_RECURSE ${unused})
 
-file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.lock")  
+file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.lock")
 file(REMOVE_RECURSE ${unused})
 
 if(EXISTS "${SOURCE_PATH}/sdk/identity/azure-identity")
