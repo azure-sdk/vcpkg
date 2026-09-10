7171b3e27db5dde57d69d95969e0d698efa4e2ab
azure-sdk <azuresdk@microsoft.com>
2026-09-10 21:40:01 +0000

    Daily vcpkg ports for storage

--- ports/azure-storage-queues-cpp/portfile.cmake
+++ ports/azure-storage-queues-cpp/portfile.cmake
@@ -4,18 +4,18 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-storage-queues_${VERSION}"
-    SHA512 d11a55a5d5b3ef7a79e20bd79364ba981e5a2d6539d3e8c3152f4c462f5e52025fe9de8f81f821fd0747e6f3143ba4ddacff1683d9ba4c1f5a1a454e9255f773
+    REF e39cdfd980a7d4d5318e4d3e16b990098e2c0213
+    SHA512 07f5748589ea9957c657ea6686980b43605815fc422a6564b49979f041bf8337c146d1faca828259220ae6a623bf1b7b6d448d38197b749cdd3d0e5ff9d6a27a
     HEAD_REF main
 )
 
 file(GLOB_RECURSE unused "${SOURCE_PATH}/cgmanifest.json")
 file(REMOVE_RECURSE ${unused})
 
-file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.toml")  
+file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.toml")
 file(REMOVE_RECURSE ${unused})
 
-file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.lock")  
+file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.lock")
 file(REMOVE_RECURSE ${unused})
 
 if(EXISTS "${SOURCE_PATH}/sdk/storage/azure-storage-queues")
